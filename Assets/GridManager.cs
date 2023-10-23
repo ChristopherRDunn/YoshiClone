0aab23611ae7bacc098afd7ee1a219bdc190d097
Christopher Dunn <crdunn@shaw.ca>
2023-10-22 17:51:55 -0700

    Clean up logic and start on dropping pieces logic

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -12,19 +12,60 @@ public class GridManager : MonoBehaviour
     [SerializeField]
     private float tileSize = 1;
     private const string GRID_LAYER_NAME = "Default";
+    private float updateInterval = 1.0f / 32.0f;  // Update every 1/32 seconds
+    private float lastUpdateTime = 0;
     PlaySpace[,] playGrid;
+    public float count = 0;
+
+    private Dictionary<PieceType, string> pieceTypeNames;
 
     // Start is called before the first frame update
     void Start()
     {
         GeneratePlayGrid();
         Invoke("GenerateStuff", 1);
+
+        pieceTypeNames = new Dictionary<PieceType, string>
+        {
+            { PieceType.Empty, "Empty" },
+            { PieceType.Blooper, "Blooper" },
+            { PieceType.Piranha, "Piranha" },
+            { PieceType.Boo, "Boo" },
+            { PieceType.Goomba, "Goomba" },
+            { PieceType.BottomShell, "BottomShell" },
+            { PieceType.TopShell, "TopShell" },
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float currentTime = Time.time;
+
+        // This check will ensure the game runs at 32 frames per second
+        if (currentTime - lastUpdateTime >= updateInterval)
+        {
+            // If nothing is falling, we'll generate a new set of pieces instead
+            bool hasMovingPieces = false;
+            lastUpdateTime = currentTime;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    // Don't update static pieces
+                    if (!playGrid[row, col].isFalling) continue;
+                    hasMovingPieces = true;
+
+                    dropPiece(row, col);
+                }
+            }
+            
+            if (!hasMovingPieces)
+            {
+                Debug.Log("TODO: Generated new units");
+            }
+        }
     }
 
     void GeneratePlayGrid()
@@ -51,6 +92,7 @@ public class GridManager : MonoBehaviour
             for (int j = 0; j < cols; j++)
             {
                 playGrid[i, j].piece = PieceType.Goomba;
+                playGrid[i, j].hasNewPiece = true;
             }
         }
     }
@@ -63,33 +105,8 @@ public class GridManager : MonoBehaviour
             {
                 if (playGrid[row, col].hasNewPiece) {
                     Destroy(playGrid[row, col].gameObject);
-                    GameObject prefab;
-                    switch (playGrid[row, col].piece)
-                    {
-                        case PieceType.Blooper:
-                            prefab = (GameObject)Instantiate(Resources.Load("Blooper"));
-                            break;
-                        case PieceType.Goomba:
-                            prefab = (GameObject)Instantiate(Resources.Load("Goomba"));
-                            break;
-                        case PieceType.Boo:
-                            prefab = (GameObject)Instantiate(Resources.Load("Boo"));
-                            break;
-                        case PieceType.Pirahna:
-                            prefab = (GameObject)Instantiate(Resources.Load("Pirahna"));
-                            break;
-                        case PieceType.TopShell:
-                            prefab = (GameObject)Instantiate(Resources.Load("TopShell"));
-                            break;
-                        case PieceType.BottomShell:
-                            prefab = (GameObject)Instantiate(Resources.Load("BottomShell"));
-                            break;
-                        default:
-                            prefab = new GameObject();
-                            break;
-                    }
-
-                    prefab.transform.position = new Vector3(getX(col), getY(row));
+                    GameObject prefab = (GameObject)Instantiate(Resources.Load(pieceTypeNames[playGrid[row, col].piece]));
+                    prefab.transform.position = new Vector3(getXCoord(col), getYCoord(row));
                     playGrid[row, col].gameObject = prefab;
                     playGrid[row, col].hasNewPiece = false;
                 }
@@ -97,12 +114,25 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    float getY(float y)
+    // Controls the logic about moving a piece down and checking for matches
+    void dropPiece(int x, int y)
+    {
+        // If there's no tile below them, move them down
+        
+        // If they match the block below them, shatter them both
+        // If they're a top shell, check for a matching bottom shell
+        //   * If found, shatter everything in between
+        //   * Else just shatter it
+        // If they're over the top, Game over
+        // If there is a block below them, place it and stop it from falling
+    }
+
+    float getYCoord(float y)
     {
         return (y * -tileSize) + 3.5f;
     }
 
-    float getX(float x)
+    float getXCoord(float x)
     {
         return (x * tileSize) + 0.5f;
     }
@@ -114,20 +144,25 @@ internal class PlaySpace
     public Vector2 position;
     public GameObject gameObject;
     public bool hasNewPiece;
+    public bool isFalling;
 
     public PlaySpace(PieceType initPiece, Vector2 initPosition)
     {
         piece = initPiece;
         position = initPosition;
         gameObject = new GameObject();
-        hasNewPiece = false;
+        if (piece != PieceType.Empty)
+        {
+            hasNewPiece = true;
+            isFalling = true;  
+        } 
     }
 }
 
 public enum PieceType {
     Empty,
     Blooper,
-    Pirahna,
+    Piranha,
     Boo,
     Goomba,
     BottomShell,
